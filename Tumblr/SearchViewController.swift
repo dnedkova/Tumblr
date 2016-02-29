7c743bf718d0c1f1df30b5b3dba779b3676fcc1e
Diana Nedkova <dnedkova@lyft.com>
2016-02-28 21:08:41 -0800

    loading animation

--- Tumblr/SearchViewController.swift
+++ Tumblr/SearchViewController.swift
@@ -10,15 +10,54 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var loadingImageView: UIImageView!
+    
+    @IBOutlet weak var searchFeedImage: UIImageView!
+    var loading_1: UIImage!
+    var loading_2: UIImage!
+    var loading_3: UIImage!
+    var images: [UIImage]!
+    var animatedImage: UIImage!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        loading_1 = UIImage(named: "loading-1")
+        loading_2 = UIImage(named: "loading-2")
+        loading_3 = UIImage(named: "loading-3")
+        
+        images = [loading_1, loading_2, loading_3]
+        animatedImage = UIImage.animatedImageWithImages(images, duration: 1.0)
+        loadingImageView.image = animatedImage
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        searchFeedImage.alpha = 0
+        loadingImageView.alpha = 1
+       
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        UIView.animateWithDuration(1.2, animations: { () -> Void in
+            self.loadingImageView.alpha = 0
+         
+            }) { (Bool) -> Void in
+                
+                self.searchFeedImage.alpha = 1
+        }
+        
     }
     
 
